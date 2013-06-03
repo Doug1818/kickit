40b71714212c289abbc2941390df6a1e0ed7a15f
Doug Raicek <doug.raicek@gmail.com>
2013-06-03 14:30:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kickit::Application.config.secret_token = '20eff3cf30b77f3316a89815667d3cb1ea5522e74021c8c1d7c24df5d33858886f6f93b84aaa44ca537053b7c880d1bcefc497aa11ab9fd6a35afd2506848a58'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kickit::Application.config.session_store :cookie_store, key: '_kickit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kickit::Application.config.session_store :active_record_store
