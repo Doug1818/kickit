f1e36b0f8a3526cbb6f7600abc5a5c058b7009db
Doug Raicek <doug.raicek@gmail.com>
2013-06-04 17:29:57 -0400

    Bring back caching but set header home link to no ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
 	helper :all
 	protect_from_forgery
 	before_filter :prepare_for_mobile
-	before_filter :set_cache_buster
 
 	private
 	def mobile_device?
@@ -17,11 +16,5 @@ class ApplicationController < ActionController::Base
 	def prepare_for_mobile
 		session[:mobile_param] = params[:mobile] if params[:mobile]
     	request.format = :mobile if mobile_device?
-	end
-
-	def set_cache_buster
-		response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
-		response.headers["Pragma"] = "no-cache"
-		response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
 	end
 end
