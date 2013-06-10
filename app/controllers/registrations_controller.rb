328a95d688b6f6742575fcae6e49be41127f5811
Doug Raicek <doug.raicek@gmail.com>
2013-06-10 19:26:28 -0400

    Add welcome flash message on signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,6 +12,7 @@ skip_before_filter :require_no_authentication, :only => [:new, :create]
     end
     if @user.save
       sign_in @user
+      flash[:success] = "Welcome! You have signed up successfully."
       redirect_to root_path
       UserMailer.new_user_welcome(@user).deliver
     else
