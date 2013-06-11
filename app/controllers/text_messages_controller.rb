b0de236480a6e95c6221cfaf667010e843f7402e
Doug Raicek <doug.raicek@gmail.com>
2013-06-11 16:21:48 -0400

    Tweak receive texts action

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -6,7 +6,8 @@ class TextMessagesController < ApplicationController
 		from_number = params["From"][2..-1]
 		@user = User.find_by_phone(from_number)
 		@day = @user.days.find_by_date(Date.today)
-		@sent_text = User.sent_texts.create(message: message)
+		#binding.pry
+		@sent_text = @user.sent_texts.create(message: message)
 		if @user.start_date <= Date.today && @user.end_date >= Date.today
 			if @sent_text.message.upcase == "Y" || @sent_text.message.upcase == "N"
 				if @day.result?
@@ -15,6 +16,7 @@ class TextMessagesController < ApplicationController
 				else
 					response = Twilio::TwiML::Response.new { |r| r.Sms "Thanks for checking in today!" }
 					render :xml => response.text
+					@day.update_attributes(result: 1)
 				end
 			else
 				response = Twilio::TwiML::Response.new { |r| r.Sms "Invalid reply. Please enter either 'Y' or 'N'. Thank you!" }
