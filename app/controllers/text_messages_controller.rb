9652df5801b3d6e14a66eee495e32e29ca7bf15f
Doug Raicek <doug.raicek@gmail.com>
2013-06-12 11:35:35 -0400

    Make text check-ins receivable only in a 24 hour window

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -5,12 +5,16 @@ class TextMessagesController < ApplicationController
 		message = params["Body"]
 		from_number = params["From"][2..-1]
 		@user = User.find_by_phone(from_number)
-		@day = @user.days.find_by_date(Date.today)
 		@sent_text = @user.sent_texts.create(message: message)
-		if !@day.nil? #User is in an active program (@day is not nil)
+		if Time.now.hour >= 9 #Because check-in reminder comes in at 9am on day + 1
+			@day = @user.days.find_by_date(Date.today - 1)
+		else
+			@day = @user.days.find_by_date(Date.today - 2)
+		end
+		if !@day.nil? && Time.now >= @day + 1 + 9.hours && Time.now <= @day + 2 + 9.hours #User is in an active program (@day is not nil) and in the 24hr check-in window
 			if message.upcase == "Y" || message.upcase == "N"
 				if @day.result?
-					response = Twilio::TwiML::Response.new { |r| r.Sms "You have already checked in." }
+					response = Twilio::TwiML::Response.new { |r| r.Sms "You have already checked in today." }
 					render :xml => response.text
 				else
 					if message.upcase == "Y"
@@ -22,12 +26,20 @@ class TextMessagesController < ApplicationController
 					render :xml => response.text
 				end
 			else
-				response = Twilio::TwiML::Response.new { |r| r.Sms "Invalid reply. Please enter either 'Y' or 'N'. Thank you!" }
+				response = Twilio::TwiML::Response.new { |r| r.Sms "Invalid check-in. Please enter either 'Y' or 'N'. Thank you!" }
 				render :xml => response.text
 			end
 		else
-			response = Twilio::TwiML::Response.new { |r| r.Sms "You are not signed up for a Kick it program.  To start a new program go to http://www.kick-it-now.com" }
-			render :xml => response.text
+			if @user.start_date > Date.today
+				response = Twilio::TwiML::Response.new { |r| r.Sms "Your Kick it program hasn't started yet." }
+				render :xml => response.text
+			elsif @user.start_date == Date.today
+				response = Twilio::TwiML::Response.new { |r| r.Sms "Your first check-in will be tomorrow morning." }
+				render :xml => response.text
+			else
+				response = Twilio::TwiML::Response.new { |r| r.Sms "You are not signed up for a Kick it program.  To start a new program go to http://www.kick-it-now.com" }
+				render :xml => response.text
+			end
 		end
 	end
 end
