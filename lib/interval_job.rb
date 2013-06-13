d15fd0c1d0a438a2e2a550c42db8e78ee13ef345
Doug Raicek <doug.raicek@gmail.com>
2013-06-12 21:32:44 -0400

    Change interval job to specific phone number as test

--- lib/interval_job.rb
+++ lib/interval_job.rb
@@ -2,16 +2,14 @@ module IntervalModule
 
 class IntervalJob
 	def perform
-		User.active_program.all.each do |user|
-			number_to_send_to = user.phone
-			@twilio_client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
-		 
-			@twilio_client.account.sms.messages.create(
-				:from => "+1#{ENV['TWILIO_PHONE_NUMBER']}",
-				:to => number_to_send_to,
-				:body => "Reminder test"
-			)
-		end
+		number_to_send_to = "9175879211"
+		@twilio_client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
+	 
+		@twilio_client.account.sms.messages.create(
+			:from => "+1#{ENV['TWILIO_PHONE_NUMBER']}",
+			:to => number_to_send_to,
+			:body => "Reminder test"
+		)
 	end
 end
 
