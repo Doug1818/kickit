e17a1aba305a32f9af9669049f519aa731e4d27b
Doug Raicek <doug.raicek@gmail.com>
2013-06-13 11:14:10 -0400

    Try another lib test 2

--- lib/interval_job.rb
+++ lib/interval_job.rb
@@ -3,13 +3,10 @@ require File.expand_path('../../config/environment', __FILE__)
 
 module IntervalModule
 
-class IntervalJob
+class IntervalJob < Struct.new(:number_to_send_to, :twilio_client, :number_sent_from)
 	def perform
-		number_to_send_to = "9175879211"
-		@twilio_client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
-	 
 		@twilio_client.account.sms.messages.create(
-			:from => "+1#{ENV['TWILIO_PHONE_NUMBER']}",
+			:from => number_sent_from,
 			:to => number_to_send_to,
 			:body => "Reminder test"
 		)
