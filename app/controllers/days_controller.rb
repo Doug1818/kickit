e84ee5dfa41f550286e5488e98589455b06624d3
Doug Raicek <doug.raicek@gmail.com>
2013-06-24 00:51:59 -0400

    Fix failure action bug in days controller

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -18,7 +18,7 @@ class DaysController < ApplicationController
 
   def failure
   	@day = Day.find(params[:id])
-  	@day.update_attributes(result: 1)
+  	@day.update_attributes(result: 2)
   	redirect_to root_path
   end
 end
