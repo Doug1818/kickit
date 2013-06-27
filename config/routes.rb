f38b8a25e42c4dc05657c817ad53ca6ac2568934
Doug Raicek <doug.raicek@gmail.com>
2013-06-26 20:25:26 -0400

    Add devise admins model to work with rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Kickit::Application.routes.draw do
   
+  devise_for :admins
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users, controllers: { registrations: "user_registrations" }
   #devise_scope :user do
   #  root to: "devise/registrations#new"
   #end
