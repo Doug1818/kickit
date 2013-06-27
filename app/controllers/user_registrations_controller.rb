b7e98fa0ad873b1eae757ecb8e79a132ec291a79
Doug Raicek <doug.raicek@gmail.com>
2013-06-26 20:29:15 -0400

    Change registrations_controller to user_registrations_controller

---
+++ app/controllers/user_registrations_controller.rb
@@ -0,0 +1,23 @@
+class UserRegistrationsController < Devise::RegistrationsController
+skip_before_filter :require_no_authentication, :only => [:new, :create]
+
+  def new
+    @user = User.new
+    if user_signed_in?
+      @days = current_user.days
+      @today = params[:month] ? Date.parse(params[:month]) : Date.current
+    end
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome! You have signed up successfully."
+      redirect_to root_path
+      UserMailer.new_user_welcome(@user).deliver
+    else
+      render 'new'
+    end
+  end
+end
