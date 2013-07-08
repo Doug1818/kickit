6b8fd09278a62c46bf4f8c2a0060751d0044274b
Doug Raicek <doug.raicek@gmail.com>
2013-07-08 18:30:17 -0400

    Add text responses in text_msgs_controller to received_text model

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -2,56 +2,74 @@ class TextMessagesController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 
 	def receive
-		message = params["Body"]
+		sent_msg = params["Body"]
 		from_number = params["From"][2..-1]
 		@user = User.find_by_phone(from_number)
-		@sent_text = @user.sent_texts.create(message: message)
+		@sent_text = @user.sent_texts.create(message: sent_msg)
 		if Time.zone.now.hour >= 9 # Because check-in reminder comes in at 9am (13:00 UTC) on day + 1
 			@day = @user.days.find_by_date(Date.current - 1)
 		else
 			@day = @user.days.find_by_date(Date.current - 2)
 		end
 		if !@day.nil? && Time.zone.now >= @day.date + 1 + 9.hours && Time.zone.now <= @day.date + 2 + 9.hours # User is in an active program (@day is not nil) and in the 24hr check-in window
-			if message.upcase == "Y" || message.upcase == "N"
+			if sent_msg.upcase == "Y" || sent_msg.upcase == "N"
 				if @day.result == 1 || @day.result == 2
 					if Time.zone.now.hour >= 0 && Time.zone.now.hour < 9 # If it's between midnight and 9am
-						response = Twilio::TwiML::Response.new { |r| r.Sms "You have already checked in for #{(Date.current - 1).strftime("%a, %b %d")}. To check in for #{Date.current.strftime("%a, %b %d")} go to http://www.kick-it-now.com or check in by text after 9am." }
+						received_msg = "You have already checked in for #{(Date.current - 1).strftime("%a, %b %d")}. To check in for #{Date.current.strftime("%a, %b %d")} go to http://www.kick-it-now.com or check in by text after 9am."
+						response = Twilio::TwiML::Response.new { |r| r.Sms received_msg }
 						render :xml => response.text
+						received_text = user.received_texts.create(message: received_msg)
 					else
-						response = Twilio::TwiML::Response.new { |r| r.Sms "You have already checked in today." }
+						received_msg = "You have already checked in today."
+						response = Twilio::TwiML::Response.new { |r| r.Sms received_msg }
 						render :xml => response.text
+						received_text = user.received_texts.create(message: received_msg)
 					end
 				elsif @day.result == 4
 					if Time.zone.now.hour >= 0 && Time.zone.now.hour < 9 # If it's between midnight and 9am
-						response = Twilio::TwiML::Response.new { |r| r.Sms "Check-in window for #{(Date.current - 1).strftime("%a, %b %d")} is closed. To check in for #{Date.current.strftime("%a, %b %d")} go to http://www.kick-it-now.com or check in by text after 9am." }
+						received_msg = "Check-in window for #{(Date.current - 1).strftime("%a, %b %d")} is closed. To check in for #{Date.current.strftime("%a, %b %d")} go to http://www.kick-it-now.com or check in by text after 9am."
+						response = Twilio::TwiML::Response.new { |r| r.Sms received_msg }
 						render :xml => response.text
+						received_text = user.received_texts.create(message: received_msg)
 					else
-						response = Twilio::TwiML::Response.new { |r| r.Sms "You have already checked in today." }
+						received_msg = "You have already checked in today."
+						response = Twilio::TwiML::Response.new { |r| r.Sms received_msg }
 						render :xml => response.text
+						received_text = user.received_texts.create(message: received_msg)
 					end
 				elsif @day.result == 3
-					if message.upcase == "Y"
+					if sent_msg.upcase == "Y"
 						@day.update_attributes(result: 1)
-					elsif message.upcase == "N"
+					elsif sent_msg.upcase == "N"
 						@day.update_attributes(result: 2)
 					end
-					response = Twilio::TwiML::Response.new { |r| r.Sms "Thanks for checking in today!" }
+					received_msg = "Thanks for checking in today!"
+					response = Twilio::TwiML::Response.new { |r| r.Sms received_msg }
 					render :xml => response.text
+					received_text = user.received_texts.create(message: received_msg)
 				end
 			else
-				response = Twilio::TwiML::Response.new { |r| r.Sms "Invalid check-in. Please enter either 'Y' or 'N'. Thank you!" }
+				received_msg = "Invalid check-in. Please enter either 'Y' or 'N'. Thank you!"
+				response = Twilio::TwiML::Response.new { |r| r.Sms received_msg }
 				render :xml => response.text
+				received_text = user.received_texts.create(message: received_msg)
 			end
 		else
 			if @user.start_date > Date.current
-				response = Twilio::TwiML::Response.new { |r| r.Sms "Your Kick it program hasn't started yet." }
+				received_msg = "Your Kick it program hasn't started yet."
+				response = Twilio::TwiML::Response.new { |r| r.Sms received_msg }
 				render :xml => response.text
+				received_text = user.received_texts.create(message: received_msg)
 			elsif @user.start_date == Date.current
-				response = Twilio::TwiML::Response.new { |r| r.Sms "Your first check-in will be tomorrow morning." }
+				received_msg = "Your first check-in will be tomorrow morning."
+				response = Twilio::TwiML::Response.new { |r| r.Sms received_msg }
 				render :xml => response.text
+				received_text = user.received_texts.create(message: received_msg)
 			else
-				response = Twilio::TwiML::Response.new { |r| r.Sms "You are not signed up for a Kick it program.  To start a new program go to http://www.kick-it-now.com" }
+				received_msg = "You are not signed up for a Kick it program.  To start a new program go to http://www.kick-it-now.com"
+				response = Twilio::TwiML::Response.new { |r| r.Sms received_msg }
 				render :xml => response.text
+				received_text = user.received_texts.create(message: received_msg)
 			end
 		end
 	end
