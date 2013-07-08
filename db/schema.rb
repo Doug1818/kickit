5e6f9ec72b3a2e542681043bb4fd1d270a983260
Doug Raicek <doug.raicek@gmail.com>
2013-07-08 15:02:00 -0400

    Reinstall rails_admin

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,19 @@ ActiveRecord::Schema.define(:version => 20130708163423) do
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 8
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
+
   create_table "received_texts", :force => true do |t|
     t.integer  "user_id"
     t.string   "message"
