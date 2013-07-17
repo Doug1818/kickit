57e45d0eba30a24f3e63e77851ebeac7dd89cd55
Doug Raicek <doug.raicek@gmail.com>
2013-07-17 15:05:16 -0400

    Fix program bug in static_pages_controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
 	def home
 		@lead = Lead.new
 		@user = User.new
-		if user_signed_in?
+		if user_signed_in? && current_user.program != nil
 			@days = current_user.program.days
 	  		@today = params[:month] ? Date.parse(params[:month]) : Date.current
   		end
