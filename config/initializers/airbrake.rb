417e929cc6d3af487ffdbe5868d84cf798a5d00e
Doug Raicek <doug.raicek@gmail.com>
2013-07-25 17:49:31 -0400

    Set config.use_system_ssl_cert_chain to true in airbrake.rb

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,6 @@
 Airbrake.configure do |config|
   config.api_key = ENV['AIRBRAKE_API_KEY']
+  config.use_system_ssl_cert_chain = true
   config.secure = true
   config.rescue_rake_exceptions = true
 end
