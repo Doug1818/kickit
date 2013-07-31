d3c82bb8dd0ef6c7c1c6d8f2ddcf3d244739bc16
Doug Raicek <doug.raicek@gmail.com>
2013-07-31 15:18:32 -0400

    Switch create comment success flash order

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,8 @@ class CommentsController < ApplicationController
     @day = Day.find(params[:comment][:day_id])
     @reminder = @day.comments.build(content: params[:comment][:content])
     if @reminder.save
-      flash[:success] = "Comment added"
       redirect_to root_path
+      flash[:success] = "Comment added"
     else
       render :nothing => true
     end
