d432477bdf19d53175450fef7c2726368f9132cc
Doug Raicek <doug.raicek@gmail.com>
2013-08-05 11:53:13 -0400

    Change supporter update copy

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,7 +1,7 @@
 class Week < ActiveRecord::Base
   attr_accessible :start_date, :end_date, :free_days, :week
   belongs_to :program
-  scope :closed, where("end_date <= ?", Date.current - 2)
+  scope :closed, where("end_date <= ?", Date.current - 2).order("start_date desc")
 
   def days
   	p = Program.find(self.program_id)
