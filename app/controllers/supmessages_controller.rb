eea865977a8ba86bc358bb732aa68999e81d06e9
Doug Raicek <doug.raicek@gmail.com>
2013-08-06 09:57:54 -0400

    Fix token bug in supmessages controller

--- app/controllers/supmessages_controller.rb
+++ app/controllers/supmessages_controller.rb
@@ -2,12 +2,17 @@ class SupmessagesController < ApplicationController
 	def create
 		@supporter = Supporter.find(params[:supmessage][:supporter_id])
 		@supmessage = @supporter.supmessages.build(content: params[:supmessage][:content])
+    @token = generate_token(@supporter)
     if @supmessage.save
       flash[:success] = "Message sent"
-      redirect_to supporter_path(@supporter)
+      redirect_to supporter_path(@token)
       UserMailer.supporter_badge(@supmessage).deliver
     else
       render 'supporters/show'
     end
 	end
+
+  def generate_token(supporter)
+    Base64.encode64("#{supporter.id}X#{supporter.first_name}#{supporter.relationship}").gsub(/[^0-9a-z]/i, "")
+  end
 end
