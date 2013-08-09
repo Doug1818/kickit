576cf5208167b88af16de7e68550f440fae16586
Doug Raicek <doug.raicek@gmail.com>
2013-08-09 10:59:42 -0400

    Tweak error page styling

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,8 +1,19 @@
 class ErrorsController < ApplicationController
-  def error_404
-  	@not_found_path = params[:not_found]
-  end
+	layout :web_layout, only: [:error_404, :error_500]
+  
+	def error_404
+		@not_found_path = params[:not_found]
+	end
 
-  def error_500
-  end
+	def error_500
+	end
+
+	private
+    def web_layout
+      if params[:format] != "mobile"
+        "user_web"
+      else
+        "application"
+      end
+    end
 end
