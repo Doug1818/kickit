5c1bb00282aaafc19f4f489eaac16b2367577f71
Doug Raicek <doug.raicek@gmail.com>
2013-08-14 19:10:17 -0400

    Change config.assets.compile to true in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Kickit::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true #or false?
 
   # Generate digests for assets URLs
   config.assets.digest = true
